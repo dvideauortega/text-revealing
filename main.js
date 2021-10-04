e0e8e5c1e72d7789ec14583aa752984d4215d975
David Videau Ortega <d.videau.ortega@gmail.com>
2021-10-03 23:17:04 -0300

    Rewritten class with new names and a simpler structure

--- main.js
+++ main.js
@@ -1,45 +1,62 @@
-class RevealContainer {
+class ViewContainer {
 
-    constructor() {
-        this.#detectLinks();
-        this.#attachAnimationListeners();
+    #container = null;
+    #views = null;
+
+    #settings = {
+        selectors: {
+            CONTAINER_SELECTOR: ".view-container",
+            VIEW_SELECTOR: ".view[data-view-name]",
+            LINK_SELECTOR: "[data-view-link-to]",
+            LINK_ATTRIBUTE_NAME: "data-view-link-to"
+        }
     }
 
-    #detectLinks() {
-        document.querySelectorAll("[slide-link]").forEach(link => {
-            link.addEventListener("click", this.#linkEventListener.bind(this));
+    #util = {
+        getNextViewFromLink: link => {
+            const name = link.getAttribute(this.#settings.selectors.LINK_ATTRIBUTE_NAME);
+            const selector = `${this.#settings.selectors.VIEW_SELECTOR.slice(0, -1)}='${name}']`;
+            return this.#container.querySelector(selector);
+        },
+    }
+
+    constructor(container) {
+        this.#container = container;
+        this.#views = this.#container.querySelectorAll(this.#settings.selectors.VIEW_SELECTOR);
+        this.#configure();
+    }
+
+    #configure() {
+        this.#views.forEach(view => {
+            this.#setupAnimationStart(view);
+            this.#setupAnimationEnd(view);
+            this.#setupLinks(view);
+        });
+    }
+
+    #setupAnimationStart(view) {
+        view.addEventListener("animationstart", () => 
+            view.classList.contains("active") ? view.classList.add("visible") : null
+        );
+    }
+
+    #setupAnimationEnd(view) {
+        view.addEventListener("animationend", () =>
+            !view.classList.contains("active") ? view.classList.remove("visible") : null
+        )
+    }
+
+    #setupLinks(view) {
+        view.querySelectorAll(this.#settings.selectors.LINK_SELECTOR).forEach(link => {
+            link.addEventListener("click", () => {
+                const nextView = this.#util.getNextViewFromLink(link);
+                view.classList.remove("active");
+                nextView.classList.add("active");
+            })
+            
         })
     }
 
-    #linkEventListener(event) {
-        let nextSlideName = event.target.getAttribute("slide-link");
-        let nextSlideElem = document.querySelector(`.slide[slide='${nextSlideName}']`);
-        let currentSlide = this.#getSlideFromLink(event.target);
-        currentSlide.classList.remove("active");
-        nextSlideElem.classList.add("active");
-    }
-
-    #attachAnimationListeners() {
-        document.querySelectorAll(".slide[slide]").forEach(slide => slide.addEventListener("animationend", event => {
-            let slide = this.#getSlideFromLink(event.target);
-            if (!slide.classList.contains("active"))
-                slide.classList.remove("visible");
-        }))
-
-        document.querySelectorAll(".slide[slide]").forEach(slide => slide.addEventListener("animationstart", event => {
-            let slide = this.#getSlideFromLink(event.target);
-            if (slide.classList.contains("active"))
-                slide.classList.add("visible");
-        }))
-    }
-
-    #getSlideFromLink(element) {
-        if(element.classList.contains("slide"))
-            return element;
-        return this.#getSlideFromLink(element.parentElement);
-    }
-    
-
 }
 
-new RevealContainer();
+new ViewContainer(document.querySelector(".view-container"));
