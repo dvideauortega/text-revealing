d56bb01168d297cdcc57c5a431d25ce9fddfe4d4
David Videau Ortega <d.videau.ortega@gmail.com>
2021-10-07 17:55:04 -0300

    Renamed files to remove conflict when merging main branch

--- main.js
+++ main.js
@@ -1,3 +1,83 @@
-document.querySelector(".menu-icon").addEventListener("click", () => {
-    document.querySelector(".menu").classList.toggle("active");
-})
+class ViewContainer {
+
+    #container = null;
+    #views = null;
+    #currentActivatedView = null;
+
+    #settings = {
+        selectors: {
+            CONTAINER_SELECTOR: ".view-container",
+            VIEW_SELECTOR: ".view[data-view-name]",
+            LINK_SELECTOR: "[data-view-link-to]",
+            LINK_ATTRIBUTE_NAME: "data-view-link-to",
+            DEFAULT_VIEW_ATTRIBUTE_NAME: "data-default-activated-view"
+        }
+    }
+
+    #util = {
+        getNextViewFromLink: link => {
+            const name = link.getAttribute(this.#settings.selectors.LINK_ATTRIBUTE_NAME);
+            const selector = `${this.#settings.selectors.VIEW_SELECTOR.slice(0, -1)}='${name}']`;
+            return this.#container.querySelector(selector);
+        },
+        getNextViewByName: viewName => {
+            const selector = `${this.#settings.selectors.VIEW_SELECTOR.slice(0, -1)}='${viewName}']`;
+            return this.#container.querySelector(selector);
+        }
+    }
+
+    constructor(container) {
+        this.#container = container;
+        this.#views = this.#container.querySelectorAll(this.#settings.selectors.VIEW_SELECTOR);
+        this.#configure();
+    }
+
+    #configure() {
+        this.#views.forEach(view => {
+            this.#setupAnimationStart(view);
+            this.#setupAnimationEnd(view);
+            this.#setupLinks(view);
+            !this.#currentActivatedView && view.hasAttribute(this.#settings.selectors.DEFAULT_VIEW_ATTRIBUTE_NAME) ? this.activateView(view) : null;
+        });
+    }
+
+    #setupAnimationStart(view) {
+        view.addEventListener("animationstart", () => 
+            view.classList.contains("active") ? view.classList.add("visible") : null
+        );
+    }
+
+    #setupAnimationEnd(view) {
+        view.addEventListener("animationend", () =>
+            !view.classList.contains("active") ? view.classList.remove("visible") : null
+        )
+    }
+
+    #setupLinks(view) {
+        view.querySelectorAll(this.#settings.selectors.LINK_SELECTOR).forEach(link =>
+            link.addEventListener("click", () => 
+                this.activateNextViewFromLink(link)
+            )
+        )
+    }
+
+    activateNextViewFromLink(link) {
+        const nextView = this.#util.getNextViewFromLink(link);
+        this.activateView(nextView);
+    }
+
+    activateView(view) {
+        if (this.#currentActivatedView && this.#currentActivatedView != view)
+            this.#currentActivatedView.classList.remove("active");
+        this.#currentActivatedView = view;
+        view.classList.add("active");
+    }
+
+    activateViewByName(viewName) {
+        const view = this.#util.getNextViewByName(viewName);
+        this.activateView(view);
+    }
+
+}
+
+new ViewContainer(document.querySelector(".view-container"));
