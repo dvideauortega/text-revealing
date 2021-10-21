dfa2d99cc78ee6019733289dbab43de0437ad181
David Videau <david.videau@um.edu.ar>
2021-10-21 11:42:36 -0300

    Added state change on animation end

--- refactor.js
+++ refactor.js
@@ -2,6 +2,7 @@ class View {
 
     #domElement = null;
     #isActive = false;
+    #hideEvent = new CustomEvent("hide");
 
     constructor(domElement, activeByDefault = false) {
         this.#domElement = domElement;
@@ -11,48 +12,39 @@ class View {
     }
 
     #setupAnimationStart() {
-        if (this.isActive) return;
-        console.log("animationstart?")
         this.#domElement.addEventListener("animationstart", () => {
-            this.#domElement.classList.add("visible");
+            if (!this.#isActive) this.#domElement.classList.add("visible");
         })
     }
 
     #setupAnimationEnd() {
-        if (!this.isActive) return;
-        console.log("animationend?")
         this.#domElement.addEventListener("animationend", () => {
-            this.#domElement.classList.remove("active");
+            if (this.#isActive) this.#domElement.classList.remove("visible");
+            this.#isActive = !this.#isActive;
         })
+    }
+
+    activate() {
+        this.#domElement.classList.add("active");
+    }
+
+    deactivate() {
+        this.#domElement.classList.remove("active");
     }
 
     get isActive() {
         return this.#isActive;
     }
 
-    set isActive(activationState) {
-        // Make checks for boolean values
-        this.#isActive = activationState;
-    }
-
-    activate() {
-        this.isActive = true;
-        this.#domElement.classList.add("active");
-    }
-
-    deactivate() {
-        this.isActive = false;
-        this.#domElement.classList.remove("active");
-    }
-
 }
 
 const domElement = document.querySelector(".view");
 const view = new View(domElement);
-view.activate();
+view.activate()
 document.querySelector(".testbtn").addEventListener("click", () => {
-    if (view.isActive)
+    if (view.isActive) {
         view.deactivate()
-    else
+    } else {
         view.activate();
+    }
 })
