270b4f67558c76360974d0989274a065030bf75f
David Videau Ortega <d.videau.ortega@gmail.com>
2021-10-22 09:15:30 -0300

    Testing JS animation API

--- refactor.js
+++ refactor.js
@@ -2,28 +2,45 @@ class View {
 
     #domElement = null;
     #isActive = false;
+    #activationAnimations = [];
+    #deactivationAnimations = [];
 
-    constructor(domElement, activeByDefault = false) {
+    constructor(domElement, activeByDefault = false, ...animations) {
         this.#domElement = domElement;
-        this.#isActive = activeByDefault;
-        this.#setupAnimationEnd();
+        this.#configureAnimations();
+        if (activeByDefault) this.activate();
     }
 
-    #setupAnimationEnd() {
-        this.#domElement.addEventListener("animationend", () => {
-            !this.#isActive ? this.#domElement.classList.remove("visible") : null;
+    #configureAnimations() {
+        this.#domElement.querySelectorAll(".transition-wrapper").forEach(wrapper => {
+            const activationKeyframeDefinition = [ { transform: "translateX(-100%)" }, { transform: "translateX(0%)" } ];
+            const activationKeyframeOptions = { duration: 500, iterations: 1, fill: "forwards", delay: 100 };
+            const activationKeyframeEffect = new KeyframeEffect(wrapper, activationKeyframeDefinition, activationKeyframeOptions);
+            const deactivationKeyframeDefinition = [ { transform: "translateX(0%)" }, { transform: "translateX(100%)" } ];
+            const deactivationKeyframeOptions = { duration: 500, iterations: 1, fill: "forwards", delay: 100 };
+            const deactivationKeyframeEffect = new KeyframeEffect(wrapper, deactivationKeyframeDefinition, deactivationKeyframeOptions);
+            const activationAnimation = new Animation(activationKeyframeEffect);
+            const deactivationAnimation = new Animation(deactivationKeyframeEffect);
+            activationAnimation.onfinish = () => console.log("Activation animation finished");
+            //deactivationAnimation.onfinish = () => console.log("Deactivation animation finished");
+            deactivationAnimation.onfinish = () => this.#domElement.classList.remove("visible");
+            activationAnimation.persist();
+            this.#activationAnimations.push(activationAnimation);
+            this.#deactivationAnimations.push(deactivationAnimation);
         })
     }
 
     activate() {
         this.#isActive = true;
+        this.#deactivationAnimations.forEach(animation => animation.cancel());
         this.#domElement.classList.add("visible");
-        this.#domElement.classList.add("active");
+        this.#activationAnimations.forEach(animation => animation.play());
     }
 
     deactivate() {
         this.#isActive = false;
-        this.#domElement.classList.remove("active");
+        this.#activationAnimations.forEach(animation => animation.cancel());
+        this.#deactivationAnimations.forEach(animation => animation.play());
     }
 
     get isActive() {
@@ -33,16 +50,12 @@ class View {
 }
 
 const domElement = document.querySelector(".view[data-view-name='one']");
-const domElement2 = document.querySelector(".view[data-view-name='two']");
-const view = new View(domElement);
-const view2 = new View(domElement2);
+const view = new View(domElement, true);
 view.activate();
 document.querySelector(".testbtn").addEventListener("click", () => {
-    if (view.isActive) {
+    console.log(view.isActive);
+    if (view.isActive)
         view.deactivate();
-        view2.activate();
-    } else {
-        view2.deactivate();
+    else
         view.activate();
-    }
 })
